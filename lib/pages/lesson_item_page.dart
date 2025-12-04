3bd6760c5beb49d303acec0bb34a774c33b36085
anto <antobuilds@gmail.com>
2025-12-04 13:21:11 +0100

    december 4 class - implementing audio using justaudio

--- lib/pages/lesson_item_page.dart
+++ lib/pages/lesson_item_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:w_phonics/models/phonics_character.dart';
 import 'package:w_phonics/pages/flashcard_page.dart';
 import 'package:w_phonics/pages/formation_page.dart';
+import 'package:w_phonics/widgets/phonics_item_card.dart';
+import 'package:w_phonics/widgets/sounding_section_widget.dart';
 
 class LessonItemPage extends StatefulWidget {
   const LessonItemPage({
@@ -34,10 +37,13 @@ class _LessonItemPageState extends State<LessonItemPage> {
         children: [
           //
           // Story section
-          _buildCardView(title: "Story", child: Text(phonicsCharacter.story)),
+          PhonicsItemPageCard(
+            title: "Story",
+            child: Text(phonicsCharacter.story),
+          ),
           //
           // Action Section
-          _buildCardView(
+          PhonicsItemPageCard(
             title: "Action",
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -49,7 +55,7 @@ class _LessonItemPageState extends State<LessonItemPage> {
           ),
           //
           // Flash Card
-          _buildCardView(
+          PhonicsItemPageCard(
             title: "Flash Card",
             actions: IconButton(
               onPressed: () {
@@ -88,7 +94,7 @@ class _LessonItemPageState extends State<LessonItemPage> {
           ),
           //
           // Finger tracing
-          _buildCardView(
+          PhonicsItemPageCard(
             title: "Formation",
             bottomItem: ElevatedButton(
               style: ElevatedButton.styleFrom(
@@ -106,6 +112,7 @@ class _LessonItemPageState extends State<LessonItemPage> {
               },
               child: Text("Finger tracing"),
             ),
+
             child: Center(
               child: Container(
                 decoration: BoxDecoration(
@@ -128,8 +135,14 @@ class _LessonItemPageState extends State<LessonItemPage> {
             ),
           ),
           //
+          // Sounding
+          SoundingSectionWidget(
+            phonicChar: phonicChar,
+            phonicsCharacter: phonicsCharacter,
+          ),
+          //
           // Writing
-          _buildCardView(
+          PhonicsItemPageCard(
             title: "Writing",
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -159,47 +172,96 @@ class _LessonItemPageState extends State<LessonItemPage> {
           ),
           //
           // Song
-          _buildCardView(
+          PhonicsItemPageCard(
             title: "Song",
-            child: Column(children: [Text(phonicsCharacter.songText)]),
+            child: Column(
+              children: [
+                Text(phonicsCharacter.songText),
+                SongPlayerWidget(songAsset: widget.phonicsCharacter.songAudio),
+              ],
+            ),
           ),
         ],
       ),
     );
   }
+}
 
-  Widget _buildCardView({
-    required String title,
-    required Widget child,
-    Widget? actions,
-    Widget? bottomItem,
-  }) {
+class SongPlayerWidget extends StatefulWidget {
+  const SongPlayerWidget({super.key, required this.songAsset});
+  final String songAsset;
+
+  @override
+  State<SongPlayerWidget> createState() => _SongPlayerWidgetState();
+}
+
+class _SongPlayerWidgetState extends State<SongPlayerWidget> {
+  var justAudio = AudioPlayer();
+  bool showText = false;
+  bool playing = false;
+  double currentSeconds = 0;
+  double maxSeconds = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeAudio();
+  }
+
+  Future<void> initializeAudio() async {
+    await justAudio.setAsset(widget.songAsset);
+    maxSeconds = (justAudio.duration?.inSeconds ?? 0).toDouble();
+
+    justAudio.positionStream.listen((duration) {
+      currentSeconds = duration.inSeconds.toDouble();
+
+      if(currentSeconds >= maxSeconds){
+        print("came in to the end");
+        playing = false;
+        justAudio.stop();
+      }
+
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    justAudio.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      padding: EdgeInsets.all(12),
-      margin: EdgeInsets.symmetric(vertical: 8),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                ),
-                if (actions != null) actions,
-              ],
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Slider(
+                value: currentSeconds,
+                max: maxSeconds,
+                onChanged: (position) {
+                  justAudio.seek(Duration(seconds: position.toInt()));
+                },
+              ),
+              Text(currentSeconds.toString())
+            ],
           ),
-          Container(child: child),
-
-          if (bottomItem != null) bottomItem,
+          IconButton(
+            onPressed: () {
+              if (playing) {
+                playing = false;
+                justAudio.stop();
+              } else {
+                playing = true;
+                justAudio.play();
+              }
+              setState(() {});
+            },
+            icon: Icon(playing ? Icons.pause_circle : Icons.play_circle, size: 60,),
+          ),
         ],
       ),
     );
